ef3bbb87ab97a1dc2959d2e103220751eaa1a911
Ahmar Suhail <ahmarsu@amazon.co.uk>
2025-06-24 16:55:13 +0100

    more test changes

--- input-stream/src/integrationTest/java/software/amazon/s3/analyticsaccelerator/access/SmallObjectPrefetchingIntegrationTest.java
+++ input-stream/src/integrationTest/java/software/amazon/s3/analyticsaccelerator/access/SmallObjectPrefetchingIntegrationTest.java
@@ -16,8 +16,7 @@
 package software.amazon.s3.analyticsaccelerator.access;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static software.amazon.s3.analyticsaccelerator.util.Constants.ONE_KB;
-import static software.amazon.s3.analyticsaccelerator.util.Constants.ONE_MB;
+import static software.amazon.s3.analyticsaccelerator.util.Constants.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -30,7 +29,7 @@ public class SmallObjectPrefetchingIntegrationTest extends IntegrationTestBase {
 
   @ParameterizedTest
   @MethodSource("prefetchingTest")
-  void testSmallObjectPrefetchedData(S3ClientKind s3ClientKind) throws IOException {
+  void testSmallObjectRead(S3ClientKind s3ClientKind) throws IOException {
 
     List<StreamRead> streamReads = new ArrayList<>();
     streamReads.add(new StreamRead(ONE_MB, 500 * ONE_KB));
@@ -56,6 +55,46 @@ public class SmallObjectPrefetchingIntegrationTest extends IntegrationTestBase {
       // slightly inaccurate,
       // and reports a value of 6.
       //  assertEquals(3,
+      // s3AALClientStreamReader.getS3SeekableInputStreamFactory().getMetrics().get(MetricKey.CACHE_HIT));
+    }
+  }
+
+  @ParameterizedTest
+  @MethodSource("prefetchingTest")
+  void testLargeObjectRead(S3ClientKind s3ClientKind) throws IOException {
+
+    List<StreamRead> streamReads = new ArrayList<>();
+    streamReads.add(new StreamRead(ONE_MB, 500 * ONE_KB));
+    streamReads.add(new StreamRead(3 * ONE_MB, 2 * ONE_MB));
+    streamReads.add(new StreamRead(5 * ONE_MB, 2 * ONE_MB));
+    streamReads.add(new StreamRead(500 * ONE_MB, 8 * ONE_MB));
+    streamReads.add(new StreamRead(2 * ONE_GB, 6 * ONE_MB));
+
+    StreamReadPattern streamReadPattern =
+        StreamReadPattern.builder().streamReads(streamReads).build();
+
+    try (S3AALClientStreamReader s3AALClientStreamReader =
+        this.createS3AALClientStreamReader(s3ClientKind, AALInputStreamConfigurationKind.DEFAULT)) {
+
+      // Since the object is more than 8MB, there should be no prefetching, and all blocks will have
+      // their own GET.
+      testAndCompareStreamReadPattern(
+          s3ClientKind, S3Object.RANDOM_10GB, streamReadPattern, s3AALClientStreamReader);
+
+      assertEquals(
+          5,
+          s3AALClientStreamReader
+              .getS3SeekableInputStreamFactory()
+              .getMetrics()
+              .get(MetricKey.GET_REQUEST_COUNT));
+
+      // TODO: This should be fixed with the new PhysicalIO, currently the cache hit metric is
+      // slightly inaccurate,
+      // and reports a value of 6. Expected value should be 0 as nothing is prefetched, and there is
+      // no repeated access
+      // to any blocks.
+      //       assertEquals(0,
+      //
       // s3AALClientStreamReader.getS3SeekableInputStreamFactory().getMetrics().get(MetricKey.CACHE_HIT));
     }
   }
